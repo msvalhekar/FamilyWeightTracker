ed34af9f42351ff399861135f829e7d40bdd4163
mvalhekar <mvalhekar@spiderlogic.com>
2016-03-30 19:46:12 +0530

    dashboard is now created, showing cardView items

--- app/src/main/java/com/mk/familyweighttracker/HomeActivity.java
+++ app/src/main/java/com/mk/familyweighttracker/HomeActivity.java
@@ -1,16 +1,28 @@
 package com.mk.familyweighttracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.activeandroid.ActiveAndroid;
 import com.mk.familyweighttracker.Activities.UsersListActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,17 +42,26 @@ public class HomeActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_home);
 
+        initToolbarControl();
+
+        List<DashboardItem> items = new ArrayList<>();
+        items.add(new DashboardItem("Calculate BMI", ""));
+        items.add(new DashboardItem("Track BMI", ""));
+        items.add(new DashboardItem("Calculate Pregnancy Weight", ""));
+        items.add(new DashboardItem("Track Pregnancy Weight", UsersListActivity.class.getName()));
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        DashboardItemRecyclerViewAdapter adapter = new DashboardItemRecyclerViewAdapter(this, items);
+
+        RecyclerView recyclerView = ((RecyclerView) findViewById(R.id.dashboard_item_grid));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initToolbarControl() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_home);
         setSupportActionBar(toolbar);
-
-        Button startButton = ((Button) findViewById(R.id.button_home_start));
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), UsersListActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -69,5 +90,73 @@ public class HomeActivity extends AppCompatActivity {
 //        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DashboardItemRecyclerViewAdapter extends RecyclerView.Adapter<DashboardItemRecyclerViewAdapter.DashboardItemViewHolder> {
+        private Context mContext;
+        private List<DashboardItem> mItems;
+
+        public DashboardItemRecyclerViewAdapter(Context context, List<DashboardItem> items) {
+            mContext = context;
+            mItems = items;
+        }
+
+        @Override
+        public DashboardItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View layoutView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.dashboard_item_content, null);
+            DashboardItemViewHolder vh = new DashboardItemViewHolder(layoutView);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(DashboardItemViewHolder holder, int position) {
+            DashboardItem item = mItems.get(position);
+            holder.setItem(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+
+        public class DashboardItemViewHolder extends RecyclerView.ViewHolder {
+            private TextView textView;
+            private DashboardItem mItem;
+
+            public DashboardItemViewHolder(View itemView) {
+                super(itemView);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            Class className = Class.forName(mItem.ActivityClassName);
+                            Intent intent = new Intent(getApplicationContext(), className);
+                            startActivity(intent);
+                        } catch(Exception e) {
+                            Toast.makeText(v.getContext(), "Work In Progress", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                textView = ((TextView) itemView.findViewById(R.id.grid_item_name));
+            }
+
+            public void setItem(DashboardItem item) {
+                mItem = item;
+                textView.setText(mItem.DisplayName);
+            }
+        }
+    }
+
+    private class DashboardItem {
+        public String DisplayName;
+        public String ActivityClassName;
+
+        public DashboardItem(String displayName, String activityClassName) {
+            this.DisplayName = displayName;
+            this.ActivityClassName = activityClassName;
+        }
     }
 }
