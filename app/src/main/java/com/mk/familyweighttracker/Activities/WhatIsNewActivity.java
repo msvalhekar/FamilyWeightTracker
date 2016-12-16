0ed62926f8f1694ff49cec4dcb0d5235902a5875
mvalhekar <mk@gmail.com>
2016-12-16 17:54:21 +0530

    whats new - expandable list for all versions

--- app/src/main/java/com/mk/familyweighttracker/Activities/WhatIsNewActivity.java
+++ app/src/main/java/com/mk/familyweighttracker/Activities/WhatIsNewActivity.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
+import com.mk.familyweighttracker.Adapter.WhatIsNewListAdapter;
 import com.mk.familyweighttracker.Framework.Constants;
 import com.mk.familyweighttracker.Framework.PreferenceHelper;
 import com.mk.familyweighttracker.Framework.SystemInformation;
 import com.mk.familyweighttracker.Framework.TrackerBaseActivity;
 import com.mk.familyweighttracker.HomeActivity;
 import com.mk.familyweighttracker.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mvalhekar on 15-12-2016.
@@ -29,10 +39,6 @@ public class WhatIsNewActivity extends TrackerBaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_whats_new);
         setSupportActionBar(toolbar);
 
-        TextView tv = ((TextView) findViewById(R.id.what_is_new_view));
-        tv.setText(Html.fromHtml(getWhatsNewMessage()));
-        tv.setMovementMethod(new ScrollingMovementMethod());
-
         findViewById(R.id.what_is_new_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,17 +48,85 @@ public class WhatIsNewActivity extends TrackerBaseActivity {
                 finish();
             }
         });
+
+        ExpandableListView listView = ((ExpandableListView) findViewById(R.id.what_is_new_list));
+        listView.setAdapter(new WhatIsNewListAdapter(this, getWhatsNewList()));
+        setGroupIndicatorToRight(listView);
+        listView.performItemClick(null, 0, 0);
     }
 
-    private String getWhatsNewMessage() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("<big>");
-        builder.append("<br />" + "<b>" + "Infant growth tracking" + "</b>");
-        builder.append("<br />" + "Infant physical growth can now be tracked for Weight, Height and Head Circumference.");
-        builder.append("<br />");
-        builder.append("<br />" + "<b>" + "Share growth chart" + "</b>");
-        builder.append("<br />" + "Pregnancy weight gain / Infant physical growth chart(s) can now be shared with others/social media.");
-        builder.append("</big>");
-        return builder.toString();
+    private void setGroupIndicatorToRight(ExpandableListView expListView) {
+        /* Get the screen width */
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+
+        expListView.setIndicatorBounds(width - getDipsFromPixel(35), width - getDipsFromPixel(5));
+    }
+
+    // Convert pixel to dip
+    public int getDipsFromPixel(float pixels) {
+        // Get the screen's density scale
+        final float scale = getResources().getDisplayMetrics().density;
+        // Convert the dps to pixels, based on density scale
+        return (int) (pixels * scale + 0.5f);
+    }
+
+    private List<IWhatIsNew> getWhatsNewList() {
+        List<IWhatIsNew> list = new ArrayList<>();
+
+        // 2.0.170103
+        list.add(new IWhatIsNew() {
+            @Override
+            public String getVersion() { return "2.0"; }
+
+            @Override
+            public Date getReleaseDate() {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(2017, 0, 3);
+                return calendar.getTime();
+            }
+
+            @Override
+            public Map<String, String> getFeatureMap() {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("Infant growth tracking", "Infant physical growth can now be tracked for Weight, Height and Head Circumference.");
+                map.put("Share growth chart", "Pregnancy weight gain / Infant physical growth chart(s) can now be shared with others/social media.");
+                return map;
+            }
+        });
+
+        // 1.3.161008
+        list.add(new IWhatIsNew() {
+            @Override
+            public String getVersion() { return "1.3"; }
+
+            @Override
+            public Date getReleaseDate() {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(2016, 9, 8);
+                return calendar.getTime();
+            }
+
+            @Override
+            public Map<String, String> getFeatureMap() {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("Twins", "Pregnancy with \"twins\" can also be tracked.");
+                map.put("Less calculations", "Delivery Due date is mandatory so that current week suggestion is near accurate.");
+                map.put("Current week number suggestion", "Week number is auto-calculated based on Delivery Due Date.");
+                map.put("Post-delivery reading", "Post-delivery reading can be added (completing pregnancy tracking).");
+                map.put("User name length restriction", "Restricting length of user name to 15 chars.");
+                map.put("Better UI", "Improved look and feel");
+                return map;
+            }
+        });
+
+        return list;
+    }
+
+    public interface IWhatIsNew {
+        String getVersion();
+        Date getReleaseDate();
+        Map<String, String> getFeatureMap();
     }
 }
---
+++ app/src/main/java/com/mk/familyweighttracker/Adapter/WhatIsNewListAdapter.java
@@ -0,0 +1,106 @@
+package com.mk.familyweighttracker.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.mk.familyweighttracker.Activities.WhatIsNewActivity;
+import com.mk.familyweighttracker.R;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by mvalhekar on 16-12-2016.
+ */
+
+public class WhatIsNewListAdapter extends BaseExpandableListAdapter {
+    Activity context;
+    List<WhatIsNewActivity.IWhatIsNew> list;
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+
+    public WhatIsNewListAdapter(Activity context, List<WhatIsNewActivity.IWhatIsNew> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return list.get(groupPosition).getFeatureMap().size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return list.get(groupPosition).getFeatureMap().keySet().toArray()[childPosition];
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean b, View convertView, ViewGroup parent) {
+
+        WhatIsNewActivity.IWhatIsNew whatIsNew = list.get(groupPosition);
+
+        if(convertView == null) {
+            convertView = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                    .inflate(R.layout.whats_new_group_item, null);
+        }
+
+        ((TextView) convertView.findViewById(R.id.group_item_version))
+                .setText(String.format("%s - %s",
+                        whatIsNew.getVersion(),
+                        simpleDateFormat.format(whatIsNew.getReleaseDate())));
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean b, View convertView, ViewGroup parent) {
+        WhatIsNewActivity.IWhatIsNew whatIsNew = list.get(groupPosition);
+        String title = whatIsNew.getFeatureMap().keySet().toArray()[childPosition].toString();
+        String description = whatIsNew.getFeatureMap().values().toArray()[childPosition].toString();
+
+        if(convertView == null) {
+            convertView = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                    .inflate(R.layout.whats_new_group_child_item, null);
+        }
+
+        ((TextView) convertView.findViewById(R.id.child_item_title)).setText(title);
+        ((TextView) convertView.findViewById(R.id.child_item_description)).setText(description);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
